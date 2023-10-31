3899b8a0c9fdadcee811ef081fe22a473538cb71
Ronald Salvador <140569363+Zerudosuu@users.noreply.github.com>
2023-10-31 21:44:39 +0800

    Added movement script
    Will add functionality when pressing O and P the sprite will rotate either clock or counter

--- CHROMAX/Assets/Script/PlayerMovement.cs
+++ CHROMAX/Assets/Script/PlayerMovement.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class PlayerMovement
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal;
+    private float speed = 8f;
+   [SerializeField] private float JumpPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+ 
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpPower);
+
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0.5f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+
+        }
         
+
+
     }
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+   
 }
